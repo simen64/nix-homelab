e365309449decd95105d1d2363b80d9e6adbf586
simen64 <simenmunch@gmail.com>
2026-03-06 08:26:27 +0100

    fix: upload to github

--- clan.nix
+++ clan.nix
@@ -1,11 +1,14 @@
 {
   # Ensure this is unique among all clans you want to use.
-  meta.name = "__CHANGE_ME__";
-  meta.domain = "changeme";
+  meta.name = "nix-homelab";
+  meta.domain = "simenmo.com";
 
   inventory.machines = {
-    # Define machines here.
-    # jon = { };
+    main = {
+      deploy.targetHost = "root@192.168.0.2";
+      # Define tags here (optional)
+      tags = [ ]; 
+    };
   };
 
   # Docs: See https://docs.clan.lol/services/definition/
@@ -14,30 +17,28 @@
     # Docs: https://docs.clan.lol/services/official/admin/
     # Admin service for managing machines
     # This service adds a root password and SSH access.
-    admin = {
-      roles.default.tags.all = { };
-      roles.default.settings.allowedKeys = {
-        # Insert the public key that you want to use for SSH access.
-        # All keys will have ssh access to all machines ("tags.all" means 'all machines').
-        # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
-        "admin-machine-1" = "__YOUR_PUBLIC_KEY__";
-      };
-    };
+    #admin = {
+    #  roles.default.tags.all = { };
+    #  roles.default.settings.allowedKeys = {
+    #    # Insert the public key that you want to use for SSH access.
+    #    # All keys will have ssh access to all machines ("tags.all" means 'all machines').
+    #    # Alternatively set 'users.users.root.openssh.authorizedKeys.keys' in each machine
+    #    "admin-machine-1" = "__YOUR_PUBLIC_KEY__";
+    #  };
+    #};
+    
+    simen-user = {
+      module.name = "users";
+        roles.default.tags.all = { };
 
-    # Docs: https://docs.clan.lol/services/official/zerotier/
-    # The lines below will define a zerotier network and add all machines as 'peer' to it.
-    # !!! Manual steps required:
-    #   - Define a controller machine for the zerotier network.
-    #   - Deploy the controller machine first to initialize the network.
-    zerotier = {
-      # Replace with the name (string) of your machine that you will use as zerotier-controller
-      # See: https://docs.zerotier.com/controller/
-      # Deploy this machine first to create the network secrets
-      roles.controller.machines."__YOUR_CONTROLLER__" = { };
-      # Peers of the network
-      # tags.all means 'all machines' will joined
-      roles.peer.tags.all = { };
-    };
+        roles.default.settings = {
+          user = "simen";
+          groups = [
+            "wheel" # Allow using 'sudo'
+            "networkmanager" # Allows to manage network connections.
+          ];
+        };
+      };
 
     # Docs: https://docs.clan.lol/services/official/tor/
     # Tor network provides secure, anonymous connections to your machines
@@ -51,8 +52,10 @@
   # machines/jon/configuration.nix will be automatically imported.
   # See: https://docs.clan.lol/guides/inventory/autoincludes/
   machines = {
-    # jon = { config, ... }: {
-    #   environment.systemPackages = [ pkgs.asciinema ];
-    # };
+    main = { config, pkgs, ... }: {
+      users.users.simen.openssh.authorizedKeys.keys = [
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAqDkA6c7wWbeVXWv0fh0xEB2NURnL8qudQHxDVWGPAfAAAABHNzaDo= ssh"
+      ];
+    };
   };
 }
